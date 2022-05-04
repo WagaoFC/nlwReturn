df7548475953d2da62274751e3c41d6f15bb81af
WagaoFC <wagnerferreira_07@hotmail.com>
2022-05-04 00:05:23 -0300

    metade do segundo dia

---
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -0,0 +1,26 @@
+import { CloseButton } from "../../CloseButton"
+import { FeedbackType, feedbackTypes } from "../Index"
+
+interface FeedbackContentStepProps {
+    feedbackType: FeedbackType;
+}
+
+export function FeedbackContentStep({ feedbackType }: FeedbackContentStepProps) {
+    const feedbackTypesInfo = feedbackTypes[feedbackType];
+
+    return (
+        <>
+            <header>
+                <span className="text-xl leading-6 flex items-center gap-2">
+                    <img src={feedbackTypesInfo.image.source} alt={feedbackTypesInfo.image.alt} className="w-6 h-6" />
+                    {feedbackTypesInfo.title}
+                </span>
+                <CloseButton />
+            </header>
+
+            <div className="flex py-8 gap-2 w-full">
+               
+            </div>
+        </>
+    )
+}
